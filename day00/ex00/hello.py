19a0781d9de6698680c578b5a519e1d879bab737
said mazouz <smazouz@student.1337.ma>
2024-07-29 14:46:00 +0100

    complete the first exercise

--- day00/ex00/hello.py
+++ day00/ex00/hello.py
@@ -1,12 +1,11 @@
-ft_list  = ["Hello", "tota!"]
+ft_list = ["Hello", "tata!"]
 ft_tuple = ("Hello", "toto!")
-ft_set  =  {"Hello", "tutu!"}
-ft_dict = {"Hello": "tutu!"}
+ft_set = {"Hello", "tutu!"}
+ft_dict = {"Hello" : "titi!"}
 
-
-ft_list[1] = "World!"
-ft_tuple = (ft_tuple[0], "France!")
-ft_set.remove("tutu!")
+ft_list[1] = "World"
+ft_tuple = (ft_tuple[1], "France!")
+ft_set.discard("tutu!")
 ft_set.add("Paris!")
 ft_dict["Hello"] = "42Paris!"
 
