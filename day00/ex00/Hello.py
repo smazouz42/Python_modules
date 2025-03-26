b57588656cff7b7eef3c2f1498cd22892bf124a9
smazouz42 <smazouz@student.1337.ma>
2025-03-26 23:21:22 +0000

    feat: solving the first exercise

--- day00/ex00/Hello.py
+++ day00/ex00/Hello.py
@@ -3,9 +3,9 @@ ft_tuple = ("Hello", "toto!")
 ft_set = {"Hello", "tutu!"}
 ft_dict = {"Hello" : "titi!"}
 
-ft_list[1] = "World"
-ft_tuple = (ft_tuple[1], "France!")
-ft_set.discard("tutu!")
+ft_list[1] = "World!"
+ft_tuple = (ft_tuple[0], "France!")
+ft_set.remove("tutu!")
 ft_set.add("Paris!")
 ft_dict["Hello"] = "42Paris!"
 
