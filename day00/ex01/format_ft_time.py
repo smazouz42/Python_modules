2b7d78a22735ab969e4606d01d5e0f5669f40970
smazouz42 <smazouz@student.1337.ma>
2025-03-27 01:07:52 +0000

    feat: solving the second exercise

---
+++ day00/ex01/format_ft_time.py
@@ -0,0 +1,11 @@
+import time
+import datetime
+
+current_time = time.time()
+
+scientific_notation = f"{current_time:.2e}"
+
+current_date = datetime.datetime.now().strftime("%b %d %Y")
+
+print(f"Seconds since January 1, 1970: {current_time:.4f} or {scientific_notation} in scientific notation")
+print(current_date)
